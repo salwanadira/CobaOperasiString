e263b62603024a146e323fda2342a03425b65014
SALWA NADIRA <SALWA NADIRA@LAPTOP-6GBIJ8E9>
2018-08-14 11:31:46 +0700

    Initial Commit

--- src/j11/CobaOperasiString.java
+++ src/j11/CobaOperasiString.java
@@ -11,29 +11,5 @@ public class CobaOperasiString {
         String identitas = "Salwa Nadira Adzani / X RPL 3 / 34";
         System.out.println("Identitas: " + identitas);
         
-        String x = "Operasi";
-        System.out.println("Isi variabel x : " + x);
-        System.out.println("\"" + x + "\" panjang karakter = "+ x.length());
-        
-        System.out.println("x adalah kosong : " + x.isEmpty());
-        
-        String y = "";
-        System.out.println("Isi variabel y : " + y);
-        System.out.println("y adalah kosong : " + y.isEmpty());
-        
-        System.out.println("isi x sama dengan y : " + x.equals(y));
-        
-        String z = "Operasi";
-        
-        System.out.println("isi variable z : " + z);
-        System.out.println("isi x sama dengan z z (Case Sensitive): " + x.equals(z));
-        
-        String r = "Operasi ";
-        System.out.println("isi variable r : " + r);
-        System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals(r));
-        System.out.println("isi x sama dengan z (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
-       
-        
-    
     }    
 }
