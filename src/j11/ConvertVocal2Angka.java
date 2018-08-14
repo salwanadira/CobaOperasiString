a7a401bacb310c3fb6a9c464dc2fbef8a82d0d21
SALWA NADIRA <SALWA NADIRA@LAPTOP-6GBIJ8E9>
2018-08-14 19:27:12 +0700

    Initial Commit

---
+++ src/j11/ConvertVocal2Angka.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11;
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    public static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas :" + identitas);
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+        
+    }   
+    public static void main(String[] args)
+    {
+        String identitas = "Salwa Nadira Adzani / X RPL 3 / 34";
+        tampilJudul(identitas);
+    }
+                 
+}   
+    
+    
